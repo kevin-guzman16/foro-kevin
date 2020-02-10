6e37ac3e830d02ab1428306f32cb9afeca98159c
kevin <kevinguzman0816@gmail.com>
2020-02-10 19:56:58 +0100

    haciendo login

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -94,8 +94,8 @@ class UserAuthenticator extends AbstractFormLoginAuthenticator implements Passwo
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('ultimas-publicaiones'));
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl()
